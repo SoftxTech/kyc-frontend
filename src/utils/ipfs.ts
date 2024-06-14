ee643aee12c1a990eb9f0ca2ea7b423438f86af8
AbdoTaha15 <abdo.taha15x@gmail.com>
2024-06-14 18:20:28 +0300

    add ipfs functions

---
+++ src/utils/ipfs.ts
@@ -0,0 +1,26 @@
+import { upload } from "thirdweb/storage";
+import { download } from "thirdweb/storage";
+import { createThirdwebClient } from "thirdweb";
+ 
+const client = createThirdwebClient({
+  clientId: process.env.CLIENTID || ""
+});
+
+export async function uploadFiles(files: File[]) {
+  const uris = await upload({
+      client: client,
+      files: files,
+  });
+
+  console.log(uris);
+}
+
+
+export async function downloadFile(uri: string) {
+  const file = await download({
+      client: client,
+      uri: `ipfs://${uri}`,
+  });
+
+  console.log(file);
+}
